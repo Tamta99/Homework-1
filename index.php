fa948b6125a9ce64821f129785a609d89c3e8bc2
Tamta99 <44120812+Tamta99@users.noreply.github.com>
2019-03-06 22:21:20 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+include 'Request.php';
+include 'Response.php';
+ 
+
+function get_response() {
+   $myvariable = "ar vici ras vaketeb";
+    echo ($myvariable);
+}
+get_response();
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
